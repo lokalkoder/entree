6958f57f14efdd5536e45a99f88f3b7d850adf52
joesama <joharijumali@gmail.com>
2022-09-12 22:58:39 +0800

    add theme switcher

--- stubs/resources/js/Entree/Layouts/Navigation.vue
+++ stubs/resources/js/Entree/Layouts/Navigation.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref} from 'vue';
+import {reactive, ref} from 'vue';
 import Logo from '@/Entree/Components/Logo.vue';
 import BreezeDropdown from '@/Components/Dropdown.vue';
 import BreezeDropdownLink from '@/Components/DropdownLink.vue';
@@ -9,6 +9,33 @@ import {Link} from '@inertiajs/inertia-vue3';
 import ButtonLink from "@/Entree/Components/Navigations/ButtonLink.vue";
 
 const showingNavigationDropdown = ref(false);
+
+let colorTheme = reactive({ dark: localStorage.getItem('color-theme') === 'dark' && window.matchMedia('(prefers-color-scheme: dark)').matches })
+
+const themeDark = () => {
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+            colorTheme.dark = true
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+            colorTheme.dark = false
+        }
+        // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+            colorTheme.dark = false
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+            colorTheme.dark = true
+        }
+    }
+}
 </script>
 
 <template>
@@ -55,8 +82,20 @@ const showingNavigationDropdown = ref(false);
 
                 <div class="hidden sm:flex sm:items-center sm:ml-6">
                     <!-- Settings Dropdown -->
-                    <div class="ml-3 relative">
-                        <BreezeDropdown align="right" width="48">
+                    <div class="flex ml-3 relative">
+                        <div class="flex-initial mr-4">
+                            <button id="theme-toggle" type="button"
+                                    @click="themeDark"
+                                    class="text-gray-700 dark:text-white hover:bg-gray-100 dark:bg-gray-900 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm py-2">
+                                <svg v-if="colorTheme.dark" id="theme-toggle-light-icon" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path>
+                                </svg>
+                                <svg v-else id="theme-toggle-dark-icon" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                                </svg>
+                            </button>
+                        </div>
+                        <BreezeDropdown class="flex-1" align="right" width="48">
                             <template #trigger>
                                 <span class="inline-flex rounded-md">
                                     <button type="button"
@@ -128,7 +167,10 @@ const showingNavigationDropdown = ref(false);
 
 <script>
 export default {
-    name: "Navigation"
+    name: "Navigation",
+    computed: {
+
+    }
 }
 </script>
 
