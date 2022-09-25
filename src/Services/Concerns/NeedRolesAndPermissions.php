d1358c449117e2240dd997080c4f0da5f1005b0e
joesama <joharijumali@gmail.com>
2022-09-25 22:20:16 +0800

    add helper

--- src/Services/Concerns/NeedRolesAndPermissions.php
+++ src/Services/Concerns/NeedRolesAndPermissions.php
@@ -5,6 +5,7 @@ namespace Lokalkoder\Entree\Services\Concerns;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 trait NeedRolesAndPermissions
 {
