d703e80163bb34dabeac0c11515ea6e169c90f1c
joesama <joesama@users.noreply.github.com>
2022-10-28 11:27:39 +0000

    Fix styling

--- src/Services/Concerns/NeedPermissions.php
+++ src/Services/Concerns/NeedPermissions.php
@@ -14,21 +14,20 @@ trait NeedPermissions
     {
         $permissions = collect([]);
 
-        (Permission::pluck('description', 'name'))->each(function ($permission, $key)  use ($permissions){
-
+        (Permission::pluck('description', 'name'))->each(function ($permission, $key) use ($permissions) {
             $type = explode('.', $key);
 
-            $group = $type[1] . '.' . $type[2];
+            $group = $type[1].'.'.$type[2];
 
             $set = $permissions->get($group) ?? collect([
-                    'title' => __('menu.' . $group),
-                    'item' => collect([])
-                ]);
+                'title' => __('menu.'.$group),
+                'item' => collect([]),
+            ]);
 
             if ($set !== null) {
                 $set['item']->push([
                     'id' => $key,
-                    'desc' => Str::title($type[0]) . ' ' . __('menu.' . $group)
+                    'desc' => Str::title($type[0]).' '.__('menu.'.$group),
                 ]);
 
                 $permissions->put($group, $set);
