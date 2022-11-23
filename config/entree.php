bd8ecdac9ff0f657a3e50bdeeaf62e7f2c791626
AHMED JOHARI BIN JUMALI <joharijumali@gmail.com>
2022-11-23 21:37:57 +0800

    merge 1.x to main (#5)
    * update stubs
    
    * Fix styling
    
    * format entree config
    
    * Bump dependabot/fetch-metadata from 1.3.4 to 1.3.5
    
    Bumps [dependabot/fetch-metadata](https://github.com/dependabot/fetch-metadata) from 1.3.4 to 1.3.5.
    - [Release notes](https://github.com/dependabot/fetch-metadata/releases)
    - [Commits](https://github.com/dependabot/fetch-metadata/compare/v1.3.4...v1.3.5)
    
    ---
    updated-dependencies:
    - dependency-name: dependabot/fetch-metadata
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump ramsey/composer-install from 1 to 2
    
    Bumps [ramsey/composer-install](https://github.com/ramsey/composer-install) from 1 to 2.
    - [Release notes](https://github.com/ramsey/composer-install/releases)
    - [Commits](https://github.com/ramsey/composer-install/compare/v1...v2)
    
    ---
    updated-dependencies:
    - dependency-name: ramsey/composer-install
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: joesama <joesama@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- config/entree.php
+++ config/entree.php
@@ -1,6 +1,14 @@
 <?php
 
-// config for Lokalkoder/Entree
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Lokalkoder/Entree Config
+    |--------------------------------------------------------------------------
+    |
+    | This option controls setup for 'lokalkoder/entree' functionality
+    |
+    */
+
     'enable_permission' => env('ENTREE_PERMISSION', false),
 ];
