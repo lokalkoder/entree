fd7f03655df15cbff118c467d76091ef6f6fd7fb
joesama <joharijumali@gmail.com>
2023-02-21 00:10:50 +0800

    update package dependency

--- src/Commands/EntreeInstallCommand.php
+++ src/Commands/EntreeInstallCommand.php
@@ -71,7 +71,10 @@ class EntreeInstallCommand extends InstallCommand
             return [
                 '@fortawesome/fontawesome-free' => '^6.2.0',
                 '@sweetalert2/themes' => '^5.0.12',
+                'vue-final-modal' => '^3.4.11',
+                'vue-select' => '^4.0.0-beta.5',
                 'sweetalert2' => '^11.4.8',
+                '@inertiajs/vue3' => '^1.0.2'
             ] + $packages;
         });
     }
